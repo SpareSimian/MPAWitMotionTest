3d94ac902d9e4d338c3177210fc38e21323854f1
Kenneth Porter <SpareSimian+github@SewingWitch.com>
2026-03-31 19:57:59 -0700

    Test with an actual com port.

--- MPAWitMotionTestProgram.cpp
+++ MPAWitMotionTestProgram.cpp
@@ -2,6 +2,7 @@
 #include <Timeapi.h>
 #include <conio.h> // _kbhit
 #include <chrono>
+#include <fstream>
 #include <iostream>
 #include <stdexcept>
 #include <thread>
@@ -25,10 +26,12 @@ class SerialPort
 public:
    SerialPort(const char* name);
    ~SerialPort();
-   void setBaudRate(unsigned newBaudRate);
+   bool setBaudRate(unsigned newBaudRate);
    void setLowLatency();
    void write(const std::uint8_t* buffer, std::size_t buffer_size);
    std::size_t read(std::uint8_t* buffer, std::size_t buffer_size);
+private:
+   void throwCommError(const std::string& api);
 };
 
 SerialPort::SerialPort(const char* name) :
@@ -41,7 +44,7 @@ SerialPort::SerialPort(const char* name) :
                       NULL))        // Null for Comm Devices
 {
    if (hComm == INVALID_HANDLE_VALUE)
-      throw std::runtime_error("Error in opening serial port");
+      throwCommError("opening serial port");
 }
 
 SerialPort::~SerialPort()
@@ -52,31 +55,33 @@ SerialPort::~SerialPort()
 void SerialPort::setLowLatency()
 {
    COMMTIMEOUTS commTimeouts;
-   ::GetCommTimeouts(hComm, &commTimeouts);
+   if (!::GetCommTimeouts(hComm, &commTimeouts))
+      throwCommError("GetCommTimeouts");
    // this read timeout setup will return immediately with anything in
    // kernel buffer, and will wait up to 1 msec for new stuff to
    // arrive if it's empty
    commTimeouts.ReadIntervalTimeout = MAXDWORD;
    commTimeouts.ReadTotalTimeoutMultiplier = 0;
    commTimeouts.ReadTotalTimeoutConstant = 1;
-   ::SetCommTimeouts(hComm, &commTimeouts);
+   if (!::SetCommTimeouts(hComm, &commTimeouts))
+      throwCommError("GetCommTimeouts");
 }
 
-void SerialPort::setBaudRate(unsigned newBaudRate)
+// can return false if the device doesn't support the baud rate
+bool SerialPort::setBaudRate(unsigned newBaudRate)
 {
    DCB DCB_Struct_Parameter = {0};
    DCB_Struct_Parameter.DCBlength = sizeof(DCB_Struct_Parameter);
    BOOL status = GetCommState(hComm, &DCB_Struct_Parameter);
    if (!status)
-      throw std::runtime_error("GetCommState failed");
+      throwCommError("GetCommState");
    // 8N1
    DCB_Struct_Parameter.BaudRate = newBaudRate;
    DCB_Struct_Parameter.ByteSize = 8;
    DCB_Struct_Parameter.Parity   = NOPARITY;
    DCB_Struct_Parameter.StopBits = ONESTOPBIT;
    status = SetCommState(hComm, &DCB_Struct_Parameter);
-   if (!status)
-      throw std::runtime_error("SetCommState failed");
+   return 0 != status;
 }
 
 void SerialPort::write(const std::uint8_t* buffer, std::size_t buffer_size)
@@ -90,6 +95,24 @@ std::size_t SerialPort::read(std::uint8_t* buffer, std::size_t buffer_size)
    DWORD bytesRead;
    ReadFile(hComm, buffer, (DWORD)buffer_size, &bytesRead, NULL);
    return bytesRead;
+}
+
+void SerialPort::throwCommError(const std::string& api)
+{
+   const DWORD win32_error_code = GetLastError();
+   CHAR error_message[256];
+   FormatMessageA( FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                   NULL,
+                   win32_error_code,
+                   0,               
+                   error_message,   
+                   sizeof error_message,
+                   NULL
+                 );
+   std::string message(api);
+   message += " failed: ";
+   message += error_message;
+   throw std::runtime_error(message);
 }
 
 SerialPort* g_port = NULL; // for Wit callbacks
@@ -117,8 +140,14 @@ void autobaud()
    sampleReceived = false; // reset detector
    for (auto testBaud : c_uiBaud)
    {
-      std::cout << "autobaud trying %u" << testBaud << '\n';
-      g_port->setBaudRate(testBaud);
+      std::cout << "autobaud trying " << testBaud;
+      const bool baudRateSupported = g_port->setBaudRate(testBaud);
+      if (!baudRateSupported)
+      {
+         std::cout << " not supported\n";
+         continue;
+      }
+      std::cout << '\n';
       int iRetry = 2;
       do
       {
@@ -126,7 +155,7 @@ void autobaud()
          sleepMS(100); // allow time for reply
          if (sampleReceived)
          {
-            std::cout << "found at %d baud " << testBaud << '\n';
+            std::cout << "device found at baud " << testBaud << '\n';
             if (230400 != testBaud)
             {
                // try to change the device to fastest speed and see if it worked
@@ -140,7 +169,7 @@ void autobaud()
          iRetry--;
       } while (iRetry);		
    }
-   throw std::runtime_error("not found by autobaud");
+   throw std::runtime_error("device not found by autobaud");
 }
 
 typedef std::vector<std::int16_t> Sample; // array of registers
@@ -159,33 +188,53 @@ void onReply(std::uint32_t uiReg, std::uint32_t uiRegNum)
 
 int main(int argc, const char* argv[])
 {
-   if (argc < 3)
+   try
    {
-      std::cerr << "usage: MPAWitMotionTestProgram port-name log-file\n";
+      if (argc < 3)
+         throw std::runtime_error("usage: MPAWitMotionTestProgram port-name log-file");
+      LowLatency lowLatency;
+      SerialPort port(argv[1]);
+      // open the output file early to make sure we can write to it
+      std::ofstream of(argv[2]);
+      if (!of.is_open())
+         throw std::runtime_error("failed to open output file for writing");
+      g_port = &port; // for callbacks
+      std::cout << "opening serial port successful\n";
+      port.setLowLatency();
+      WitInit(WIT_PROTOCOL_MODBUS, 0x50);
+      WitSerialWriteRegister(onWrite); // register the routine to submit commands
+      WitRegisterCallBack(onReply); // register to handle response
+      WitDelayMsRegister(sleepMS); // register delay function
+      autobaud(); // requires singleton to use static callbacks!
+      WitSetBandwidth(BANDWIDTH_256HZ);
+      WitSetOutputRate(RRATE_200HZ);
+      while (!_kbhit())
+      {
+         std::uint8_t buffer[256];
+         const std::size_t bytes_read = port.read(buffer, (DWORD)sizeof buffer);
+         for (unsigned i = 0; i < bytes_read; i++)
+            WitSerialDataIn(buffer[i]);
+      }
+      WitDeInit();
+      g_port = NULL; // catch any late attempts to use this pointer
+      // dump the samples
+      for (Samples::const_iterator p = samples.begin(); p != samples.end(); ++p)
+      {
+         for (Sample::const_iterator q = p->begin(); q != p->end(); ++q)
+         {
+            of << *q;
+            if (p->begin() != q)
+               of << ',';
+         }
+         of << '\n';
+      }
+      Samples().swap(samples); // release memory by swapping with an empty vector
+      return 0;
+   }
+   catch (const std::exception& e)
+   {
+      std::cerr << e.what() << '\n';
       return 1;
    }
-   LowLatency lowLatency;
-   SerialPort port(argv[1]);
-   g_port = &port; // for callbacks
-   std::cout << "opening serial port successful\n";
-   port.setLowLatency();
-   WitInit(WIT_PROTOCOL_MODBUS, 0x50);
-   WitSerialWriteRegister(onWrite); // register the routine to submit commands
-   WitRegisterCallBack(onReply); // register to handle response
-   WitDelayMsRegister(sleepMS); // register delay function
-   autobaud(); // requires singleton to use static callbacks!
-   WitSetBandwidth(BANDWIDTH_256HZ);
-   WitSetOutputRate(RRATE_200HZ);
-   while (!_kbhit())
-   {
-      std::uint8_t buffer[256];
-      const std::size_t bytes_read = port.read(buffer, (DWORD)sizeof buffer);
-      for (unsigned i = 0; i < bytes_read; i++)
-         WitSerialDataIn(buffer[i]);
-   }
-   WitDeInit();
-   g_port = NULL; // catch any late attempts to use this pointer
-   samples.clear(); // release memory
-   return 0;
 }
 
